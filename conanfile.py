9562455bbfd900460b39f34f204e4b3ffaacbeff
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-03-27 12:55:34 +0900

    Works on Windows

--- conanfile.py
+++ conanfile.py
@@ -14,28 +14,32 @@ class LibsndfileConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
+    url_base = "http://www.mega-nerd.com/libsndfile/files/"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.shared
 
     def source(self):
-        url_base = "http://www.mega-nerd.com/libsndfile/files/"
         if self.settings.os == "Windows":
-            if self.settings.arch == "x86":
-                bin_url = url_base + "libsndfile-%s-w32.zip" % self.version
-                bin_sha256 = "a73e1a8f87207e121a78e7cfbfe758d3ad15a28a37e4ca0e96d43348d53a2a1f"
-            elif self.settings.arch == "x86_64":
-                bin_url = url_base + "libsndfile-%s-w64.zip" % self.version
-                bin_sha256 = "b885e97c797c39127d7d252be0da704a8bbdb97948b562d95cd5b8821d2b42ba"
-            else:
-                raise Exception("Binary does not exist for these settings")
-            tools.get(bin_url, sha256=bin_sha256)
+            pass
         else:
-            url = url_base + "libsndfile-%s.tar.gz" % self.version
+            url = self.url_base + "libsndfile-%s.tar.gz" % self.version
             sha256 = "1ff33929f042fa333aed1e8923aa628c3ee9e1eb85512686c55092d1e5a9dfa9"
             tools.get(url, sha256=sha256)
             os.rename("libsndfile-" + self.version, "src")
 
     def build(self):
         if self.settings.os == "Windows":
-            pass
+            if self.settings.arch == "x86":
+                bin_url = self.url_base + "libsndfile-%s-w32.zip" % self.version
+                bin_sha256 = "a73e1a8f87207e121a78e7cfbfe758d3ad15a28a37e4ca0e96d43348d53a2a1f"
+            elif self.settings.arch == "x86_64":
+                bin_url = self.url_base + "libsndfile-%s-w64.zip" % self.version
+                bin_sha256 = "b885e97c797c39127d7d252be0da704a8bbdb97948b562d95cd5b8821d2b42ba"
+            else:
+                raise Exception("Binary does not exist for these settings")
+            tools.get(bin_url, sha256=bin_sha256)
         else:
             with tools.chdir("src"):
                 env_build = AutoToolsBuildEnvironment(self)
@@ -61,3 +65,8 @@ class LibsndfileConan(ConanFile):
             self.cpp_info.libs = ["libsndfile-1"]
         else:
             self.cpp_info.libs = ["sndfile"]
+    
+    def package_id(self):
+        if (self.settings.os == "Windows"):
+            del self.info.settings.compiler
+            del self.info.settings.build_type
